487b58efa0e293d064334d80f8d0037ecc2851aa
the-wam <loicbanco@gmail.com>
2021-05-27 14:01:38 +0200

    change the paths of the files in main.py

--- main.py
+++ main.py
@@ -14,13 +14,17 @@ def main():
     # create session
     spark = new_session()
 
+    # path
+    path_to_original_csv_file = "./bor_erp.csv"
+    path_to_parquet_file = "./data/parquet/bor_erp.parquet"
+    path_to_final_result = "./data/csv/final.csv"
+
     # import data
-    df = import_csv(spark, "./bor_erp.csv")
+    df = import_csv(spark, path_to_original_csv_file)
 
     # create a parquet file with options
     partition_by = "type"
-    path_parquet_file = "./data/parquet/bor_erp.parquet"
-    data_parquet = to_parquet(spark, df, partition_by, path_parquet_file)
+    data_parquet = to_parquet(spark, df, partition_by, path_to_parquet_file)
 
     # cleaning
     data_parquet = cleainng_order(data_parquet)
@@ -29,7 +33,7 @@ def main():
     data_parquet = group_ordered(data_parquet)
 
     # save as csv
-    save_csv(data_parquet, "./data/csv/final.csv")
+    save_csv(data_parquet, path_to_final_result)
 
 
 if __name__ == "__main__":
